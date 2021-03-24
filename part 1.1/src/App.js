9d0b16c6c3c7c6656cac130e48c2bcad3cb4cce9
Anthony Brown <abrown4123@gmail.com>
2021-03-24 16:20:33 -0500

    App component before splitting up content into their own component

--- part 1.1/src/App.js
+++ part 1.1/src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 
-const Hello = (props) => (
-  <div>
-    <p>Hello {props.name}, you are {props.age} years old</p>
-  </div>
-)
-
 const App = () => {
-  const name = "George"
-  const age = Math.floor(Math.random()*100)
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name={name} age={age}/>
-      <Hello name="Debra" age={age}/>
-      <Hello name="Becky" age={age}/>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
+
 export default App;
