327f969bbc03d2a8414aa6572a93f66fbdb8279e
Anthony Brown <abrown4123@gmail.com>
2021-04-11 15:56:10 -0500

    Exercise 1.13

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -1,11 +1,17 @@
 import React, {useState} from "react";
 
-const Button = ({ handleClick }) => (
-  <div>
-    <button onClick={handleClick}>next anecdote</button>
-  </div>
-);
-  
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
+
+const Votes = ({ votes, selected }) => {
+  if (!votes[selected]) {
+    return (
+      <div>this quote has no votes</div>
+    );
+  }
+  return (
+    <div>has {votes[selected]} votes</div>
+  );
+}
 
 const App = () => {
   const anecdotes = [
@@ -16,18 +22,36 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ];
-   
+  
+  const initVotes = () => {
+    let initObj={};
+    for (let i=0; i < anecdotes.length; i++) {
+      initObj[i] = 0;
+    }
+    return initObj;
+  }
+
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(initVotes());
+  
 
   const randomQuote = () => {
     let newSlected = Math.floor(Math.random()*anecdotes.length);
     setSelected(newSlected);
   }
 
+  const quoteVote = () => {
+    setVotes({...votes, [selected]: votes[selected] + 1});
+  }
+
   return (
     <div>
       {anecdotes[selected]}
-      <Button handleClick={randomQuote}/>
+      <Votes selected={selected} votes={votes} />
+      <div>
+        <Button handleClick={quoteVote} text="vote" /> 
+        <Button handleClick={randomQuote} text="next anecdote" />
+      </div>
     </div>
   )
 }
