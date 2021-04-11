6f2ab0df6aab241a83b914173f6e5b41ccf6bf8f
Anthony Brown <abrown4123@gmail.com>
2021-04-11 14:44:28 -0500

    Exercise 1.12

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -1,11 +1,35 @@
 import React, {useState} from "react";
 
+const Button = ({ handleClick }) => (
+  <div>
+    <button onClick={handleClick}>next anecdote</button>
+  </div>
+);
+  
+
 const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ];
+   
+  const [selected, setSelected] = useState(0);
+
+  const randomQuote = () => {
+    let newSlected = Math.floor(Math.random()*anecdotes.length);
+    setSelected(newSlected);
+  }
+
   return (
     <div>
-      Start Anecdote Exercise
+      {anecdotes[selected]}
+      <Button handleClick={randomQuote}/>
     </div>
-  );
+  )
 }
 
 export default App;
